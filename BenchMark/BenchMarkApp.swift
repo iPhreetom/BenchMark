c1e6be7507d4f22c7154ed78263578bf082d7e46
iphreetom <iphreetom@gmail.com>
2022-10-30 23:21:07 +0800

    Initial Commit

---
+++ BenchMark/BenchMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchMarkApp.swift
+//  BenchMark
+//
+//  Created by F iPhreetom on 2022/10/30.
+//
+
+import SwiftUI
+
+@main
+struct BenchMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
